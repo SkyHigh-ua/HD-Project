974d2028cc13e145b45cd261325303e33eff8ea0
delver <dima.plugaturevchik@gmail.com>
2023-05-28 02:26:09 +0300

    Improved user.dao

--- server/src/DAO/users.dao.ts
+++ server/src/DAO/users.dao.ts
@@ -1,48 +1,85 @@
-import {connection} from '../datasource/db.datasource.js';
+import connection from '../datasource/db.datasource.js';
 import UserEntity from '../entities/users.entity.js';
+import {type FindManyOptions} from 'typeorm';
+
+const entityManager = connection.manager;
 
 export async function createUser(user: UserEntity): Promise<UserEntity> {
-    return connection.manager.save(UserEntity, user);
+    const newUser = entityManager.create(UserEntity, user);
+
+    return entityManager.save(newUser);
 }
 
+type UserFilter = UserEntity;
+
 export async function findAllUsers(
-    filter?: {status?: string},
+    filter?: FindManyOptions<UserEntity>, // {status?: string}
     page?: number,
     limit?: number,
-) {
-    const query = connection.createQueryBuilder(UserEntity, 'User');
+): Promise<UserEntity[]> {
+    const options: FindManyOptions<UserEntity> = {
+        ...filter,
+        skip: page && limit ? (page - 1) * limit : undefined,
+        take: limit,
+    };
 
-    if (filter?.status) {
-        query.andWhere('User.status = :status', {status: filter.status});
-    }
-
-    if (page && limit) {
-        query.skip((page - 1) * limit).take(limit);
-    }
-
-    return query.getMany();
+    return entityManager.find(UserEntity, options);
 }
 
 export async function findUserById(id: number): Promise<UserEntity | undefined> {
-    const result = await connection.manager.findOne(UserEntity, {
-        where: {id},
-    });
+    try {
+        const result = await entityManager.findOne(UserEntity, {
+            where: {id},
+        });
 
-    return result ? result : undefined;
+        return result ? result : undefined;
+    } catch (error) {
+        console.error('Error occurred while finding user by ID:', error);
+        throw error;
+    }
 }
 
-export async function findUserByUsernameAndPassword(username: string, password: string): Promise<UserEntity | undefined> {
-    const result = await connection.manager.findOne(UserEntity, {
-        where: {username, password},
-    });
+export async function findUserByUsernameAndPassword(username: string, password: string):
+Promise<UserEntity | undefined> {
+    try {
+        const result = await entityManager.findOne(UserEntity, {
+            where: {username, password},
+        });
 
-    return result ? result : undefined;
+        return result ? result : undefined;
+    } catch (error) {
+        console.error('Error occurred while finding user by Username and Password:', error);
+        throw error;
+    }
 }
 
 export async function updateUser(user: UserEntity): Promise<UserEntity> {
-    return connection.manager.save(UserEntity, user);
+    const existingUser = await entityManager.findOne(UserEntity, {
+        where: {
+            id: user.id,
+        },
+    });
+
+    if (!existingUser) {
+        throw new Error(`User with ID ${user.id} does not exist.`);
+    }
+
+    existingUser.username = user.username;
+    existingUser.firstName = user.firstName;
+    existingUser.lastName = user.lastName;
+    existingUser.email = user.email;
+    existingUser.password = user.password;
+    existingUser.token = user.token;
+
+    return entityManager.save(existingUser);
 }
 
 export async function deleteUser(id: number): Promise<void> {
-    await connection.manager.delete(UserEntity, id);
+    const existingUser = await entityManager.findOne(UserEntity, {where: {id}});
+
+    if (!existingUser) {
+        throw new Error(`User with ID ${id} does not exist.`);
+    }
+
+    await entityManager.remove(existingUser);
 }
